b50bdbdc412c4085185fb29fd4da40a6a7f8126e
justamitsaha <justamitsaha@gmail.com>
2021-05-24 08:28:59 +0530

    first commit

---
+++ src/amit/saha/a/classesFileNames/ClassFiles.java
@@ -0,0 +1,19 @@
+package amit.saha.a.classesFileNames;
+
+
+class ClassesFiles {
+
+	public String getName() {
+		System.out.println("Welcome!");
+		return "poo";
+	}
+}
+
+//Public classes must have same file names
+//Since this class is not public it can stay in a file with different name 
+class Animal2 {
+	public String getName() {
+		System.out.println("Welcome!");
+		return "poo";
+	}
+}
