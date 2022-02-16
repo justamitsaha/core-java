8d6ebe919eac60d6b5c54b6dc45f946accbcafb0
Amit Saha <472842@tcs.com>
2022-02-16 19:24:11 +0530

    Some Changes

--- core-java-CLI/5ClassDesign/H_Initialization1.java
+++ core-java-CLI/5ClassDesign/H_Initialization1.java
@@ -1,32 +1,28 @@
 // When initiating and Class which contais main() method below order is followed
-// If there is a superclass Y of X, then initialize class Y first.
-// Process all static variable declarations in the order they appear in the super class and class.
-// Process all static initializers in the order they appear in the class.
-// When the new is called then instance variables and code blocks are executed 
-// Non static methiods and blocks are executed only when new() object is created for both the class and super class
-
+// All static variable and static blocks of the Superclass are executed first
+// All static variable and static blocks of the Childclass are executed next
+// Main method starts executing next
+// When new() is called, Non static code of Superclass executed followed by Superclass constructor code
+// Non static code of Childclass is called followed by Childclass constructor code
 //Here main is in same class so the Class is loaded 1st before main method
 public class H_Initialization1 extends Animal {
-    private String name = "BestZoo";
+    static { System.out.println("Childclass static 100"); }
+    private String name = "Non static var";
     { System.out.println(name + "-"); }
-    static { System.out.println("100"); }
-
     public static void main(String[] grass) {
-        System.out.println("main method");
+        System.out.println("Main method");
         new H_Initialization1();
-        new H_Initialization1();
+        //new H_Initialization1();
     }
-    H_Initialization1 () {System.out.println("Constructor");}
+    H_Initialization1 () {System.out.println("Childclass Constructor");}
 }
-
 class Animal {
     static int count = 0;
     static {
         count += 10;
-        System.out.println(count++);
-        System.out.println(++count);
+        System.out.println("Superclass static "+ count++);
+        System.out.println("Superclass static "+ ++count);
     }
-    {
-        System.out.println("Non static super code block");
-    }
+    { System.out.println("Non static super code block"); }
+    Animal() {System.out.println("Superclass Constructor");}
 }
--- core-java-CLI/5ClassDesign/I_Initialization2.java
+++ core-java-CLI/5ClassDesign/I_Initialization2.java
@@ -1,38 +1,36 @@
 //When initiating a class which is not not main and called with new() below is the order of execution
-//1 Static variables initialized
-//2 Any static Blocks in order they show in code
-//3 Any super constructors
-//4 Any non static variables
-// Any non static clock of code
-// Constructor 
+//1 Static block of Super class gets executed
+//2 Static block of Child class gets executed
+//3 All static blocks are executed followed by contructors of Super class
+//4 All static blocks are executed followed by contructors of Child class
 
 public class I_Initialization2  {
-
    public static void main(String[] args) {
       new Monkey();
    }
 }
-
 class Primate {
-   public Primate() { System.out.println("Primate-");}
+   public Primate() { System.out.println("Primate- constructor");}
 }
-
 class Ape extends Primate {
-   public Ape() {System.out.println("Ape1-");}
-   public Ape(int fur) {System.out.println("Ape2-");}
+   public Ape() {System.out.println("Ape1- constructor");}
+   public Ape(int fur) {System.out.println("Ape2- constructor");}
+   static {System.out.println("This is is static code of Superclass Ape");}
+   {System.out.println("This is is non static code of Superclass Ape");}
 }
-
 class Monkey extends Ape {
-   private String name = "swimmy";
-   {System.out.println(name);}
    private static int COUNT = 0;
-   static {System.out.println(COUNT); }
-   {COUNT++;System.out.println(COUNT); }
+   static {System.out.println("Thsi is static code of Monkey "+ ++COUNT); }
 
+   private String name = "swimmy";
+   {System.out.println("This is non static code of Monkey "+ name); }
+   
    public Monkey() {
       // System.out.println("Constructor"); Will not compile
       super(2);
       System.out.println("Monkey Constructor");
    }
-
 }
+
+//Thumb rule Any class loading Parent must load first
+//When a parent or child is loaded Static first --> When new() called non static -->Constructor code
