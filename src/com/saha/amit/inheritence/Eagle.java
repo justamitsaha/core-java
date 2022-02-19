91d18ba6a0960f9952cc1d5527a007214dc41e9b
Amit Saha <472842@tcs.com>
2022-02-19 10:07:41 +0530

    Some Changes

--- src/com/saha/amit/inheritence/Eagle.java
+++ src/com/saha/amit/inheritence/Eagle.java
@@ -1,5 +1,8 @@
 package com.saha.amit.inheritence;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 class Bird {
 	public void fly() {
 		System.out.println("Bird is flying");
@@ -8,10 +11,13 @@ class Bird {
 		System.out.println("Bird is eating " + food + " units of food");
 	}
 	
-	Object reproduce() {
-		Object obj = new String("lays egg");
-		return obj;
-	}
+	//Since IOException is the parent of FileNotFoundException hence overriding methods won't face a problem
+	Object reproduce() throws IOException{ Object obj = new String("lays egg");return obj;}
+	
+	void hunt() throws FileNotFoundException{}
+	
+	//No issues while overriding with checked exception
+	void die() throws ArithmeticException {}
 }
 
 public class Eagle extends Bird {
@@ -20,17 +26,25 @@ public class Eagle extends Bird {
 		System.out.println("Bird is flying at " + height + " meters");
 		return height;
 	}
-//	Since the Signature is same return type should be same or a sub type of parent
-//	public int eat(int food) { // DOES NOT COMPILE
-//		System.out.println("Bird is eating " + food + " units of food");
-//		return food;
-//	}
-	// Since String is a sub type of Object this will compile
-@Override
-	String reproduce() {
-		return "lays egg";
-	}
-	public static void main(String[] args) {
-	}
 
+	/*
+	 * Since the Signature is same return type should be same or a sub type of
+	 * parent public int eat(int food) { System.out.println("Bird is eating " + food + " units of food"); return food;}
+	 */
+	
+	// Since String is a sub type of Object this will compile
+	@Override
+	String reproduce() throws FileNotFoundException{return "lays egg";}
+
+	/*
+	 * Since FileNotFoundException is a sub class of IOException below will not compile since child class may not declare
+	 * a checked exception that is new or broader than the class of any exception declared in the parent class  
+	 void hunt () throws IOException {System.out.println("hunting");} */
+	void hunt () {System.out.println("hunting");}
+	
+	
+	@Override
+	void die() throws IllegalArgumentException {}
+
+	public static void main(String[] args) {}
 }
