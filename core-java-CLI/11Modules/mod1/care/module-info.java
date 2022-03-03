4cac6940f12d2b51a2afb93e6942b0b1dfd7f8ce
justamitsaha <justamitsaha@gmail.com>
2022-03-03 11:12:18 +0530

    Some Changes

--- core-java-CLI/11Modules/mod1/care/module-info.java
+++ core-java-CLI/11Modules/mod1/care/module-info.java
@@ -1,4 +1,4 @@
-//javac -p mods -d care care/zoo/animal/care/details/*.java care/zoo/animal/care/medical/*.java care/module-info.java
+//javac -p mods -d care care/zoo/animal/care/details/*.java care/zoo/animal/care/medical/*.java care/*.java
 module zoo.animal.care {
    exports zoo.animal.care.medical;
    requires zoo.animal.feeding;
