422b561a8d32fdee1207502289250bcbf7c205a8
justamitsaha <justamitsaha@gmail.com>
2022-03-21 09:30:45 +0530

    Some changes

---
+++ src/z/e_Streams/A_CreateStream.java
@@ -0,0 +1,18 @@
+package z.e_Streams;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class A_CreateStream {
+	public static void main(String[] args) {
+		Stream<String> empty = Stream.empty(); // count = 0
+		Stream<Integer> singleElement = Stream.of(1); // count = 1
+		Stream<Integer> fromArray = Stream.of(1, 2, 3); // count = 3
+		
+		 var list = List.of("a", "b", "c");
+		 Stream<String> fromList = list.stream();
+		 
+		 Stream<Double> randoms = Stream.generate(Math::random);
+		 Stream<Integer> oddNumbers = Stream.iterate(1, n -> n + 2);
+	}
+}
