e4c95c7d4f016643a5d9d8aef359216510d2f1a2
justamitsaha <justamitsaha@gmail.com>
2022-05-20 12:58:23 +0530

    some changes

---
+++ src/k/concurrency/G_SchedulingTask.java
@@ -0,0 +1,35 @@
+package k.concurrency;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class G_SchedulingTask {
+
+	public static void main(String[] args) {
+		Runnable task = (() -> {System.out.println("This is a runnable task");});
+		Callable<String> task2 = (() -> {System.out.println("This is callable task"); return "This is callable task";});
+		
+		//ScheduledExecutorService creates a new thread in the mentioned duration 
+		ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+		service.schedule(task, 1, TimeUnit.MINUTES);
+		ScheduledFuture<String> s = service.schedule(task2, 2, TimeUnit.SECONDS);
+		System.out.println("<--------------> " + Thread.activeCount());
+		try {
+			//The duration of the get should be such that the thread is execution is complete otherwise it will throw exception
+			System.out.println(s.get(10, TimeUnit.SECONDS));
+		} catch (InterruptedException | ExecutionException | TimeoutException e) {
+			e.printStackTrace();
+		}
+		
+		Runnable task3 = (() -> {System.out.println("This is a runnable task3");});
+		service.scheduleAtFixedRate(task, 5, 1, TimeUnit.SECONDS);
+		service.scheduleWithFixedDelay(task3, 0, 2, TimeUnit.SECONDS);
+		System.out.println("<--------------> " + Thread.activeCount());
+	}
+
+}
--- src/k/concurrency/Test.java
+++ src/k/concurrency/Test.java
@@ -10,30 +10,20 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 public class Test {
-	public static void main(String[] args) {
-		Callable<Integer> task = (()-> {for(int i=0; i<50000;i++) {
-		}return 5;});
-		Callable<Integer> task1 = (() -> 5+9);
-		
-		ExecutorService service = null;
-		
-		service = Executors.newSingleThreadExecutor();
-		try {
-			List<Future<Integer>> lst = service.invokeAll(List.of(task, task1));
-			
-			System.out.println(lst.get(0).get(1, TimeUnit.SECONDS));
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ExecutionException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (TimeoutException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-	
+	private int sheepCount = 0;
+	   private void incrementAndReport() {
+	      System.out.print((sheepCount++)+" ");
+	   }
+	   public static void main(String[] args) {
+	      ExecutorService service = null;
+	      try {
+	         service = Executors.newFixedThreadPool(20);
+	         Test manager = new Test();
+	         for(int i = 0; i < 10; i++)
+	            service.submit(() -> manager.incrementAndReport());
+	      } finally {
+	         if(service != null) service.shutdown();
+	      }
+	   }
 	
 }
