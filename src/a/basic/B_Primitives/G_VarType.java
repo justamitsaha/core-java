55a742c45d05c43eec8320a86ad8ffa63bc834e7
justamitsaha@gmail.com <472842@tcs.com>
2022-06-09 21:49:50 +0530

    Some Changes

--- src/a/basic/B_Primitives/G_VarType.java
+++ src/a/basic/B_Primitives/G_VarType.java
@@ -14,3 +14,26 @@ public class G_VarType{
         System.out.println(e);
     }
 }
+
+class x {
+	//var n = 20;	//Won't compile as class level variables type must be implicit and not implied
+	
+	//void test(var x){}  //Method parameters are also implicit
+	
+	//var x = [1,2,3];
+	
+	@SuppressWarnings("unused")
+	void test() {
+		// var x;
+		var x = 200;
+		var y = 2L;
+		//x = 3_000_000_000; //won't compile as its a long 
+		y = 3L;
+		y = 2;
+		
+		//var i = {1,2,3}; // here the type on right side is inferred so won't work
+		var i = new int[] {1,2,3};
+		//var [] j = new int[] {1,2,3};
+		var s,u =7;
+	}
+}
