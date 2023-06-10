e57d5459037adcc87605f5d637a2602e789b6a45
justamitsaha <justamitsaha@gmail.com>
2023-06-10 12:11:22 +0530

    first commit

---
+++ src/main/java/com/saha/amit/New.java
@@ -0,0 +1,10 @@
+package com.saha.amit;
+
+public class New {
+    public static void main(String[] args) {
+        long lg = 780877591838024009L;
+        String st = String.valueOf(lg);
+        long lt = Long.parseLong(st);
+        System.out.println(st);
+    }
+}
---
+++ src/main/java/com/saha/amit/coreJava/functionalProgramming/stream/StreamTerminalOperations.java
@@ -0,0 +1,14 @@
+package com.saha.amit.coreJava.functionalProgramming.stream;
+
+import com.saha.amit.dto.StudentDto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamTerminalOperations {
+    public static void main(String[] args) {
+
+    }
+
+}
