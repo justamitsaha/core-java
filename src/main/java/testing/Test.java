863c16b22d614b96184f33b2deabe75d3905990c
Amit Saha <justamitsaha@gmail.com>
2025-08-12 21:03:40 +0530

    testing

--- src/main/java/testing/Test.java
+++ src/main/java/testing/Test.java
@@ -4,26 +4,40 @@ package testing;
 public class Test {
 
     public static void main(String[] args) {
-        Person p = new Person();
-        p.name = "Amit";
-        System.out.println(p);     //Person{name='Amit'}
-        upgrade(p);
-        System.out.println(p);     //Person{name='Sachin'}  value changing
+        Box b = new Box(1,1,1);
+        changeValue(b.height);
+        System.out.println(b.height);
+        changeValue(b);
+        System.out.println(b.height);
+        changeObject(b);
+        System.out.println(b.height);
+
     }
 
-    static void upgrade(Person per) {
-        per.name = "Sachin";
+    static void changeValue(int i) {
+        i++;
+    }
+
+    static void changeValue(Box b) {
+        b.height++;
+    }
+
+    static void changeObject(Box b){
+        b = new Box(10,10,10);
     }
 }
 
-class Person {
-    String name;
 
-    @Override
-    public String toString() {
-        return "Person{" + "name='" + name + '\'' + '}';
+class Box {
+    int height, breath, length;
+
+    public Box(int height, int breath, int length) {
+        this.height = height;
+        this.breath = breath;
+        this.length = length;
     }
 }
+
 
 
 
