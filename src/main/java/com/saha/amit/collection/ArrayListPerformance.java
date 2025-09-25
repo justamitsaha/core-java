621ea6c0ddcd82b10ce704f182aa9b6703249e55
= <=>
2025-09-25 21:39:44 +0530

    Doc update

--- src/main/java/com/saha/amit/collection/ArrayListPerformance.java
+++ src/main/java/com/saha/amit/collection/ArrayListPerformance.java
@@ -6,37 +6,42 @@ import java.util.List;
 
 public class ArrayListPerformance {
     public static void main(String[] args) {
-        int counter = 100_000;
-//        ArrayList<String> arrayList = new ArrayList<>();
-//        arrayList.add("Amit");
-//        arrayList.add("Amit");
-//        arrayList.add("Amit");
-//        updateList(arrayList,counter);
+        int counter = 900_000;
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Amit");
+        arrayList.add("Amit");
+        arrayList.add("Amit");
 
         LinkedList<String> linkedList = new LinkedList<>();
         linkedList.add("Amit");
         linkedList.add("Amit");
         linkedList.add("Amit");
-        updateList(linkedList,counter);
-
+        updateListWithCounter(arrayList,linkedList,counter);
     }
 
-    public static void updateList(List<String> list, int counter){
-        long start = System.currentTimeMillis();
+    /*
+    In theory list should take more time but we are using index this advantage which Linked list has
+     */
+    public static void updateListWithCounter(ArrayList<String> list1, LinkedList<String> list2, int counter){
+        long start1 = System.currentTimeMillis();
         for (int i =0; i<counter; i++){
-            list.add(Math.round(list.size()/2),"Hello");
+            //list1.add(Math.round(list1.size()/2),"Hello");
+            //list1.add(i,"hello");
+            list1.addFirst("hello");
         }
-        long end = System.currentTimeMillis();
-        System.out.println("Time taken " +(end-start));
-    }
+        long end1 = System.currentTimeMillis();
+        System.out.println("Time taken " +(end1-start1));
 
-
-    public static void updateLinkedList(List<String> list, int counter){
-        long start = System.currentTimeMillis();
+        long start2 = System.currentTimeMillis();
         for (int i =0; i<counter; i++){
-            list.add(Math.round(list.size()/2),"Hello");
+            //list2.add(Math.round(list2.size()/2),"Hello");
+            //list2.add(i,"hello");
+            list2.addFirst("hello");
         }
-        long end = System.currentTimeMillis();
-        System.out.println("Time taken " +(end-start));
+        long end2 = System.currentTimeMillis();
+        System.out.println("Time taken " +(end2-start2));
     }
+
+
+
 }
