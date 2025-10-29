b39137e46002e2ac9cf6388d8d436897c53bf3ca
= <=>
2025-10-29 07:30:54 +0530

    Doc update

--- src/main/java/com/saha/amit/basic/A_BasicOperations.java
+++ src/main/java/com/saha/amit/basic/A_BasicOperations.java
@@ -12,7 +12,40 @@ public class A_BasicOperations {
         // Only i initialized not j also can use un-initialized local variables Class level variables get default values
         int i, j = 8;
         int k = 8, l = 8; //both initialized
+        logger.info("k *= 2 equals: {} ", k *= 2);
+        logger.info("k += 4 equals: {} ", k += 4);
+        logger.info("k -= 4 equals: {} ", k -= 4);
+        logger.info("k /= 2 equals: {} ", k /= 2);
 
+        callByValue();
+        incrementDecement();
+
+    }
+
+
+    public static void incrementDecement() {
+        int i = 5;
+        i = i++ + ++i;
+        logger.info("Value of i++ + ++i is:{}", i);
+        int a = 1;
+        a = a++ + a + ++a;
+        logger.info("Value of a++ +a+ ++a is:{}", a);
+
+        int x = 10;
+        int y = (x = 5) + 2;
+        logger.info("value of x and y is {} , {}", x, y);
+
+        i = 0;
+        logger.info("value of i after i = 3 * 5 is {}", i = 3 * 5);
+        logger.info("i = {}", i);
+
+        x = 0;
+        boolean b = (x > 0) && (++x > 0);
+        logger.info(String.valueOf(x));
+    }
+
+    public static void callByValue() {
+        int k = 8;
         // Call be Value k is not changed
         logger.info("Value of K: {}", k);  // value 8
         doubleValue(k);
@@ -22,33 +55,6 @@ public class A_BasicOperations {
         Employee employee = new Employee("Amit", 20);
         changeEmployee(employee);
         logger.info("Employee details: {}", employee.toString()); //value changed
-
-        logger.info("k *= 2 equals: {} ", k *= 2);
-        logger.info("k += 4 equals: {} ", k += 4);
-        logger.info("k -= 4 equals: {} ", k -= 4);
-        logger.info("k /= 2 equals: {} ", k /= 2);
-
-        int i = 5;
-        int j = i++ + ++i;
-        System.out.println(j);
-        int a = 1;
-        a = a++ + a + ++a;
-        System.out.println(a);
-
-        int x = 10;
-        int y = (x = 5) + 2;
-        System.out.println(y + ", " + x);
-
-        int i = 0;
-        System.out.println(i = 3 * 5);
-        System.out.println(i);
-
-        int x = 0;
-        boolean b = (x > 0) && (++x > 0);
-        System.out.println(x);
-
-
-
     }
 
     public static void doubleValue(int i) {
@@ -58,7 +64,6 @@ public class A_BasicOperations {
     public static void changeEmployee(Employee employee) {
         employee.setName("Adam");
         employee.setSalary(34);
-
     }
 
 
